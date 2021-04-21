5b442623eb46b1dcc9d7d54e55fc099077c5da6d
Gilles30 <lorison.gilles@gmail.com>
2021-04-20 21:24:45 -0300

    (feat) taking class 2

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+import "./database";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Olá NLW 05",
+  });
+});
+
+app.post("/users", (request, response) => {
+  return response.json({
+    message: "usuário salvo com sucesso",
+  });
+});
+
+app.listen(3333, () => console.log("server is running on port 3333"));
